cdb10821bf4a9259d518f8a3f7f18daa21478af2
xinglongjian <xinglongjian@qq.com>
2018-03-17 14:11:35 +0800

    add grade

---
+++ pages/grade/index.js
@@ -0,0 +1,85 @@
+// pages/grade/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movies: [
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    ],
+    grade:{
+      name:'E81班',
+      organ:'学而思xxx',
+      course:'数学夏季版',
+      openDate:'2018-1-1',
+      closeDate:'2018-4-3',
+      gradeStatus:'进行中',
+      classRoom:'XX大厦三层301房间',
+      canDelay:'NO',// 是否允许延期
+      canTranfer:"NO",// 是否可以转让 
+      finishedNum:'10', //已上
+      allNum:'20',//一共
+      leftNum:'10',// 剩下
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
